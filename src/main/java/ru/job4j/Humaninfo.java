cdb76e2e8d3e916ec3723d1b673c1081c8ed0848
hincu <hincujava@gmail.com>
2024-07-30 17:26:15 +0300

    3.6 Применение примитивных переменных на жизненных факторах, отправка на GitHub.

---
+++ src/main/java/ru/job4j/Humaninfo.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Humaninfo {
+    public static void main(String[] args) {
+        byte age = 19;
+        System.out.println(age);
+        char sex = 'M';
+        System.out.println(sex);
+        float height = 1.83f;
+        System.out.println(height);
+        int bricks = 1000000;
+        System.out.println(bricks);
+        double bacteria = 100000000000L;
+        System.out.println(bacteria);
+        double lightyear = 10000000000000000L;
+        System.out.println(lightyear);
+        boolean work = true;
+        System.out.println(work);
+        long bytes = 1000000000000000000L;
+        System.out.println(bytes);
+    }
+}
