91bf3f6b58961cc8d8c26781af6f9cc3fa82113d
hincu <hincujava@gmail.com>
2024-07-30 17:40:57 +0300

    3.6.1 Внесение коррективов Humaninfo, отправка на GitHub.

--- src/main/java/ru/job4j/Humaninfo.java
+++ src/main/java/ru/job4j/Humaninfo.java
@@ -2,21 +2,22 @@ package ru.job4j;
 
 public class Humaninfo {
     public static void main(String[] args) {
+        System.out.println("---Информация---");
         byte age = 19;
-        System.out.println(age);
+        System.out.println("Возвраст: " + age);
         char sex = 'M';
-        System.out.println(sex);
+        System.out.println("Пол: " + sex);
         float height = 1.83f;
-        System.out.println(height);
+        System.out.println("Рост: " + height);
         int bricks = 1000000;
-        System.out.println(bricks);
+        System.out.println("Количество кирпичей: " + bricks);
         double bacteria = 100000000000L;
-        System.out.println(bacteria);
+        System.out.println("КОЕ/г: " + bacteria);
         double lightyear = 10000000000000000L;
-        System.out.println(lightyear);
+        System.out.println("Сколько километров в 1057 св.г. : " + lightyear);
         boolean work = true;
-        System.out.println(work);
+        System.out.println("Разрешение на работу: " + work);
         long bytes = 1000000000000000000L;
-        System.out.println(bytes);
+        System.out.println("Размер файла (bytes): " + bytes);
     }
 }
