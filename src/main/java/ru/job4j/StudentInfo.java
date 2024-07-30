d68aaed0cc32982b32121daabe2ea04731a75141
hincu <hincujava@gmail.com>
2024-07-30 14:07:40 +0300

    V2 3.4. Info@Main@Multiple@StudentInfo-изменения на github.

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Hincu");
+        System.out.println("24.02.2005");
+
+    }
+}
