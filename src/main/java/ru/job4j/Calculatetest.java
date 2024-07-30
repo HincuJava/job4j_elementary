467a4519c1d350c7498176c3eb31586c86b797dd
hincu <hincujava@gmail.com>
2024-07-30 16:45:59 +0300

    3.5 Первое использование примитивных переменных, отправка на GitHub.

---
+++ src/main/java/ru/job4j/Calculatetest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+public class Calculatetest {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int lenght =  100500;
+        System.out.println(lenght);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        int age = 18;
+        System.out.println(age);
+        float high = 78.0332F;
+        System.out.println(high);
+        boolean baby = false;
+        System.out.println(baby);
+    }
+}
