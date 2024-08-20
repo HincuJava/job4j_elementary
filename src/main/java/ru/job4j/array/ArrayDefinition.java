7945ab769c768d10204f23d39216ad516a0fb182
hincu <hincujava@gmail.com>
2024-08-20 16:34:49 +0300

    § 6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,11 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Jane";
+        names[2] = "Doe";
+        names[3] = "Jack";
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
